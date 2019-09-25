70a1974d701a92e371353721ee37a0603d8971d8
Vince Au <vinceau09@gmail.com>
2019-09-25 17:52:21 +1000

    Use xml feed

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const https = require('https');
+const parseString = require('xml2js').parseString;
 
 const fail = (status, error, reject, callback) => {
   const result = { status, error };
@@ -11,7 +12,7 @@ module.exports = (endpoint = '/', callback) => {
     endpoint = '/' + endpoint;
   }
 
-  const url = `https://medium.com${endpoint}${endpoint.indexOf('?') === -1  ? '?' : '&' }format=json`;
+  const url = `https://medium.com/feed${endpoint}`;
 
   return new Promise((resolve, reject) => https.get(url, res => {
     if (callback && callback.write instanceof Function) {
@@ -21,19 +22,26 @@ module.exports = (endpoint = '/', callback) => {
     res.statusCode === 200 || fail(res.statusCode, res.statusMessage, reject, callback);
 
     let data = '';
-
     res.on('data', chunk => (data += chunk));
 
     res.on('end', () => {
+      // console.log(data);
       try {
-        data = data.substr(data.indexOf('{'));
-        data = JSON.parse(data) || {};
+        parseString(data, function (err, result) {
+          if (err) {
+            reject(err);
+            return;
+          }
+          // console.log(JSON.stringify(result));
+          const posts = result.rss.channel[0].item;
+      //   data = data.substr(data.indexOf('{'));
+      //   data = JSON.parse(data) || {};
 
-        const posts = data.payload && data.payload.posts
-          || data.payload && data.payload.references
-          && data.payload.references.Post
-          && Object.keys(data.payload.references.Post)
-            .map(key => data.payload.references.Post[key]);
+      //   const posts = data.payload && data.payload.posts
+      //     || data.payload && data.payload.references
+      //     && data.payload.references.Post
+      //     && Object.keys(data.payload.references.Post)
+      //       .map(key => data.payload.references.Post[key]);
 
         if (posts) {
           const result = { status: 200, response: posts };
@@ -42,6 +50,7 @@ module.exports = (endpoint = '/', callback) => {
         } else {
           fail(500, 'Could not parse the resource. Medium\'s JSON format might have changed.', reject, callback);
         }
+      });
 
       } catch (error) {
         fail(500, error.message, reject, callback);
